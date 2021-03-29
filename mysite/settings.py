d747e4a9e9de9290233599154de184af2661fbb2
Bongilou <daferumx@gmail.com>
2021-03-29 13:29:22 +0200

    Templates modificados para mostrar post desde base de datos2.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6s*ogpe$ozn=z&)989oo7%ub=f@f!-!ij%3t=_pl9@i)+*vrdp'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
